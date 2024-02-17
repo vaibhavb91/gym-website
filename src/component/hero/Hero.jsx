a08376b615bef5e1d8f826fdfd6044e7f3ccfedf
vaibhavb91 <vaibhavb.ek@gmail.com>
2024-02-17 10:40:19 +0530

    hero section done

--- src/component/hero/Hero.jsx
+++ src/component/hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./hero.css";
 import Header from "../header/Header";
+import heroImage from "../../assets/hero_image.png";
+import heroImageBack from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
 const Hero = () => {
   return (
     <div className="hero">
@@ -10,8 +14,61 @@ const Hero = () => {
           <div></div>
           <span>the best fitness club in the town</span>
         </div>
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>your</span>
+          </div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+          <div>
+            <span>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
+          </div>
+        </div>
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>expert coachs</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
       </div>
-      <div className="right-h">right</div>
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+        <div className="heart-rate">
+          <img src={Heart} alt="Heart" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+        <img src={heroImage} alt="heroImage" className="hero-image" />
+        <img
+          src={heroImageBack}
+          alt="heroImageBack"
+          className="hero-image-back"
+        />
+        <div className="calories">
+          <img src={Calories} alt="Calories" />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 Kcal</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
