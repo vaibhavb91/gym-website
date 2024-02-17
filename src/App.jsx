475f994fd34bbfce3c75d642d4c79f23b57225f0
vaibhavb91 <vaibhavb.ek@gmail.com>
2024-02-17 15:33:41 +0530

    programa and reason component section is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import "./App.css";
 import Hero from "./component/hero/Hero";
+import Programs from "./component/programs/Programs";
+import Reason from "./component/reason/Reason";
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Programs />
+      <Reason />
     </div>
   );
 }
